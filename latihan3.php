34da326e51f394b69e59712ec92068280ae01cb5
Kristinnn12 <145428035+Kristinnn12@users.noreply.github.com>
2023-12-08 15:00:36 +0700

    Add files via upload

---
+++ latihan3.php
@@ -0,0 +1,23 @@
+<?php
+$mahasiswa = ["Joni", "123456", "Sistem Informasi", "joni@gmail.com"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <ul>
+        <li><?= $mahasiswa[0]; ?></li>
+        <li><?= $mahasiswa[1]; ?></li>
+        <li><?= $mahasiswa[2]; ?></li>
+        <ht><?= $mahasiswa[3]; ?></ht>
+
+</ul>
+
+</body>
+</html>
