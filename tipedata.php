550678e2985a2c5da4525fcf744c82e0d552dcbe
Kristinnn12 <145428035+Kristinnn12@users.noreply.github.com>
2023-12-08 15:09:14 +0700

    Add files via upload

---
+++ tipedata.php
@@ -0,0 +1,14 @@
+<?php
+
+$nrp = "2230803188";
+$nama ="Kristin";
+$umur = 19;
+$nilai= 75.75;
+
+echo "nrp : " . $nrp . "</br>";
+echo "nama: $nama</br>";
+print "umur :" .$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("nilai :%.3f</br>", $nilai);
+
+?>
